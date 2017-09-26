ce80c7f7165c71839b1bad0fa17754350d6423f8
MikeGoz <gozdz@onet.pl>
2017-09-26 16:06:52 +0200

    poprawka Task 1.8

--- program.js
+++ program.js
@@ -6,18 +6,27 @@ var server = http.createServer();
 
 server.on('request', function (request, response) {
   response.setHeader("Content-Type", "text/html; charset=utf-8");
-  
+   
+  if (request.url === '/css/styles.css') {
+    response.writeHead(200, {'Content-type' : 'text/css'});
+    var fileContents = fs.readFileSync('./css/styles.css', {encoding: 'utf8'});
+    response.write(fileContents);
+    //response.end();
+  }
   if (request.method === 'GET' && request.url === '/test') {
     fs.readFile('./index.html', 'utf-8', function(err, data){
       response.write(data);
       response.end();
-    });  
+    });
   } else {
-    response.statusCode = 404;
+    //response.statusCode = 404;
     fs.readFile('img/error.jpg', function(err, data) {
-      response.writeHead(200, {'Content-Type' : 'image/jpg'});
-      response.end(data);
+      //response.writeHead(200, {'Content-Type' : 'image/jpg'});
+      //data.type = "image/jpeg";
+      response.write(data);   
+      response.end(data); 
     });  
   }    
 });   
 server.listen(8080);
+
